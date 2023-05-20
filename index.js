0ad1ab90c2b6b7e1261385022efa878d2064cc56
Patrick Okafor Chibuike <Patrickpeters911@gmail.com>
2023-05-20 14:39:25 +0100

    add media queries for responsiveness

--- index.js
+++ index.js
@@ -11,85 +11,71 @@ const pseudoTime = document.getElementById("faketime");
 let date = new Date();
 let hour = date.getHours();
 let minutes = date.getMinutes();
-          
+
 time.value = `${hour}:${minutes}`;
-pseudoTime.value = time.value;    
+pseudoTime.value = time.value;
 
-time.addEventListener("change", () => 
-{
+time.addEventListener("change", () => {
     let inputValue = time.value;
-    pseudoTime.value = inputValue; 
-}); 
+    pseudoTime.value = inputValue;
+});
 
-timeZone.addEventListener("change", () => 
-{
+timeZone.addEventListener("change", () => {
     let inputValue = time.value;
-    pseudoTime.value = inputValue; 
-}); 
+    pseudoTime.value = inputValue;
+});
 
-button.addEventListener("click", () =>
-{
+button.addEventListener("click", () => {
     let inputValue = time.value;
-    let getHour = Number(inputValue.split(":")[0]); //split the time into hour, where the splitting point is the double colon ":", [0] is the left side
-    let getMinutes = Number(inputValue.split(":")[1]); //split the time into minute, [1] is the right side of the double colon ":";
+    let getHour = Number(inputValue.split(":")[0]);
+    let getMinutes = Number(inputValue.split(":")[1]); 
 
     let enterTime = document.querySelector(".enter-time");
 
-
     if (getHour == "" || getMinutes == "") {
-    //    let enterTime = document.querySelector(".enter-time");
-       enterTime.textContent = "Please enter a valid time";
-       enterTime.style.color = "red";
+        enterTime.textContent = "Please enter a valid time";
+        enterTime.style.color = "red";
         return;
     }
-    
+
     enterTime.style.color = "#fff"
     enterTime.textContent = "Enter the time";
 
     const selectTimeZoneOption = timeZone.options[timeZone.selectedIndex];
-    const timezoneTextContent = selectTimeZoneOption.textContent; 
+    const timezoneTextContent = selectTimeZoneOption.textContent;
     const selectCountryOption = countries.options[countries.selectedIndex];
     const countryTextContent = selectCountryOption.textContent;
 
-    let result = document.querySelector("h4"); 
+    let result = document.querySelector("h4");
 
-
-    if (timeZone.value > countries.value)
-   {
+    if (timeZone.value > countries.value) {
         getHour = +getHour - (+timeZone.value) + +countries.value;
-        if (getHour.toString().length == 1) 
-        {
+        if (getHour.toString().length == 1) {
             getHour = `0${getHour}`;
         }
-        if (getMinutes.toString().length == 1)
-        {
+        if (getMinutes.toString().length == 1) {
             getMinutes = `0${getMinutes}`;
         }
         result.textContent = `${inputValue} ${timezoneTextContent}, when converted to the time in ${countryTextContent} is: ${getHour}:${getMinutes}`;
-   }
+    }
 
-   else if(timeZone.value < countries.value)
-   {
-        if (getHour.toString().length == 1)
-        {
-            getHour = `0${getHour}`; 
+    else if (timeZone.value < countries.value) {
+        if (getHour.toString().length == 1) {
+            getHour = `0${getHour}`;
         }
 
-        if (getMinutes.toString().length == 1)
-        {
+        if (getMinutes.toString().length == 1) {
             getMinutes = `0${getMinutes}`;
         }
         getHour = +getHour + (+countries.value) - +timeZone.value;
         result.textContent = `${inputValue} ${timezoneTextContent}, when converted to the time in ${countryTextContent} is: ${getHour}:${getMinutes}`;
-   }
-   else
-        if (getHour.toString().length == 1)
-        {
+    }
+    else
+        if (getHour.toString().length == 1) {
             getHour = `0${getHour}`;
         }
-        if (getMinutes.toString().length == 1)
-        {
-            getMinutes = `0${getMinutes}`;
-        }
-        result.textContent = `${inputValue} ${timezoneTextContent}, when converted to the time in ${countryTextContent} is: ${getHour}:${getMinutes}`;
+    if (getMinutes.toString().length == 1) {
+        getMinutes = `0${getMinutes}`;
+    }
+    result.textContent = `${inputValue} ${timezoneTextContent}, when converted to the time in ${countryTextContent} is: ${getHour}:${getMinutes}`;
 });
